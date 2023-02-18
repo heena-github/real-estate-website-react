54fb7df19663cbc7ce94925cb1d73cbd64560235
aamir socatali <hk61900@gmail.com>
2023-02-18 20:58:50 +0000

    created pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-2xl bg-red-400' >hello</h1>
-    </div>
+    <>
+     <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/sign-in" element={<Signin/>}/>
+        <Route path="/sign-up" element={<Signup/>}/>
+        <Route path="/forgot-password" element={<ForgotPassword/>}/>
+        <Route path="/offers" element={<Offers/>}/>
+      </Routes>
+     </Router>
+    </>
   );
 }
 
