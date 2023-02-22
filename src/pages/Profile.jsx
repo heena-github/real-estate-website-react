a6354461690918a92566bdd9741f2d3eaf474647
aamir socatali <hk61900@gmail.com>
2023-02-22 18:53:18 +0000

    created my listings section to the profile page and listingitem component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,20 @@
 import { getAuth, updateProfile } from 'firebase/auth'
-import { doc, updateDoc } from 'firebase/firestore';
-import React, { useState } from 'react';
+import { collection, doc, getDocs, orderBy, query, updateDoc, where } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import { toast } from 'react-toastify';
 import { db } from '../firebase';
 import { FcHome } from "react-icons/fc";
 import { Link } from 'react-router-dom';
+import ListingItem from '../components/ListingItem';
 
 
 
 const Profile = () => {
   const navigate = useNavigate()
-  const auth = getAuth()
+  const auth = getAuth();
+  const [listings,setListings] = useState(null)
+  const [loading,setLoading] = useState(true)
   const [changeDetails,setChangeDetails] = useState(false)
   const [formData,setFormData] = useState({
     name: auth.currentUser.displayName,
@@ -52,6 +55,29 @@ const Profile = () => {
       toast.error('Could not update the profile details')
     }
   }
+
+  useEffect(()=>{
+    async function fetchUserListings(){
+     
+      const listingRef = collection(db,'listings');
+      const q = query(
+        listingRef,
+        where('userRef','==',auth.currentUser.uid),
+        orderBy('timestamp','desc')
+      );
+      const querySnap = await getDocs(q);
+      let listings = [];
+      querySnap.forEach((doc)=>{
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        })
+      })
+      setListings(listings)
+      setLoading(false)
+    }
+    fetchUserListings();
+  },[auth.currentUser.uid])
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -113,6 +139,23 @@ const Profile = () => {
 
         </div>
       </section>
+
+      <div className="max-w-6xl px-3 mt-6 mx-auto">
+      {!loading && listings.length > 0 && (
+          <>
+            <h2 className="text-2xl text-center font-semibold">My Listings</h2>
+            <ul>
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  id={listing.id}
+                  listing={listing.data}
+                />
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
     </>
   )
 }
